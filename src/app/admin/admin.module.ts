6afd23cf43a85437634446889234fc6c8fdd24ef
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2020-03-17 15:11:56 -0400

    clean up unused files

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,14 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
 
 @NgModule({
-  declarations: [AdminComponent],
   imports: [
     CommonModule,
     RouterModule,
     AdminRoutingModule,
-  ]
+  ],
 })
 export class AdminModule { }
